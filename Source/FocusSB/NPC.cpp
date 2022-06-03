d5a88cd85f48f9ed1d6419a12cda28f0ffae6280
jwkapple <jwkorange@gmail.com>
2022-06-03 15:51:13 +0900

    [Add] NPC : Basic setup & interact function

--- Source/FocusSB/NPC.cpp
+++ Source/FocusSB/NPC.cpp
@@ -3,12 +3,66 @@
 
 #include "NPC.h"
 
+#include "FocusSBCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
 // Sets default values
 ANPC::ANPC()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PlayerCharacter = Cast<AFocusSBCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	if(PlayerCharacter != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ECSentry:: Found PlayerCharacter"));
+	}
+	
+	mStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("NPCSM"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> NPC_SM(TEXT("/Game/StarterContent/Shapes/Shape_NarrowCapsule.Shape_NarrowCapsule"));
+	if(NPC_SM.Succeeded())
+	{
+		mStaticMeshComponent->SetStaticMesh(NPC_SM.Object);
+	}
+
+	mStaticMeshComponent->SetCollisionProfileName(FName("BlockAll"));
+	SetRootComponent(mStaticMeshComponent);
+	
+	static ConstructorHelpers::FObjectFinder<UMaterialInstance> NPC_MI(TEXT("/Game/UnveilPlatform_MI.UnveilPlatform_MI"));
+	if(NPC_MI.Succeeded())
+	{
+		mMaterialInstance = NPC_MI.Object;	
+	}
+
+	mHitBox = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HITBOX"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> HitBox_SM(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if(HitBox_SM.Succeeded())
+	{
+		mHitBox->SetStaticMesh(HitBox_SM.Object);
+		mHitBox->SetVisibility(false);
+		mHitBox->SetCollisionProfileName(FName("NPC"));
+		mHitBox->SetupAttachment(GetRootComponent());
+		mHitBox->OnComponentBeginOverlap.AddDynamic(this, &ANPC::OnBeginOverlap);
+		mHitBox->OnComponentEndOverlap.AddDynamic(this, &ANPC::OnEndOverlap);
+	}
+
+}
+
+void ANPC::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Cast<AFocusSBCharacter>(OtherActor)->SetActionUIVisible(true);
+}
+
+void ANPC::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	Cast<AFocusSBCharacter>(OtherActor)->SetActionUIVisible(false);
+}
+
+void ANPC::Interact()
+{
+	
 }
 
 // Called when the game starts or when spawned
--- Source/FocusSB/NPC.h
+++ Source/FocusSB/NPC.h
@@ -19,8 +19,32 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category = Player)
+	class AFocusSBCharacter* PlayerCharacter;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	virtual void Interact();
+
+public:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* mStaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UMaterialInstance* mMaterialInstance;
+
+	UPROPERTY(VisibleAnywhere, Category = HitBox)
+	UStaticMeshComponent* mHitBox;
+	
 };
+
+
+
